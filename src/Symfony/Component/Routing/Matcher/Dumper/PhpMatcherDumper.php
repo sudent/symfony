8b074e8a390f4463ac0ad1cb3522dc33fb25b88a
Fabien Potencier <fabien.potencier@gmail.com>
2012-04-25 04:54:52 +0200

    [Routing] fixed indentation of dumped collections

--- src/Symfony/Component/Routing/Matcher/Dumper/PhpMatcherDumper.php
+++ src/Symfony/Component/Routing/Matcher/Dumper/PhpMatcherDumper.php
@@ -130,11 +130,12 @@ EOF;
     private function compileRoutes(RouteCollection $routes, $supportsRedirections)
     {
         $code = '';
-        $indent = 0;
         $collections = $this->groupRoutesByHostnameRegex($routes)->getRoot();
         $fetchedHostname = false;
 
         foreach ($collections as $collection) {
+            $indent = 0;
+
             if ($regex = $collection->get('hostname_regex')) {
                 if (!$fetchedHostname) {
                     $code .= "        \$hostname = \$this->context->getHost();\n\n";
@@ -151,7 +152,6 @@ EOF;
             $code .= $this->indentCode($lines, $indent);
 
             if ($regex) {
-                $indent = 0;
                 $code .= "        }\n\n";
             }
         }
@@ -184,7 +184,6 @@ EOF;
         }
 
         if ($optimizable) {
-            $indent = 0;
             $code .= "        }\n\n";
         }
 
